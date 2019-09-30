98b930b0d25a92187c1c1a5fc82a692b9965c044
GerbenSchreuders <32164035+GerbenSchreuders@users.noreply.github.com>
2019-09-30 21:55:56 +0200

    Add files via upload

---
+++ Opdracht 3.9.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main (){
+	USER := bufio.NewReader(os.Stdin)
+	fmt.Print("Gebruikersnaam: ")
+	RS, _ := USER.ReadString('\n')
+
+	WW := bufio.NewReader(os.Stdin)
+	fmt.Print("Wachtwoord: ")
+	WS, _ := WW.ReadString('\n')
+
+	if RS == "Bezoeker1" {
+		if WS == "12345" {
+			fmt.Print("Correct wachtwoord")
+		} else {
+			fmt.Print("Incorrect wachtwoord, probeer opnieuw")
+		}
+
+	} else if RS == "Bezoeker2" {
+		if WS == "asdfg" {
+			fmt.Print("Correct wachtwoord")
+		} else {
+			fmt.Print("Incorrect wachtwoord, probeer opnieuw")
+		}
+	} else if RS == "Bezoeker 3" {
+		if WS == "hjkl" {
+			fmt.Print("Correct wachtwoord")
+		} else {
+			fmt.Print("Incorrect wachtwoord, probeer opnieuw")
+		}
+	} else {
+		fmt.Print("Gebruikersnaam onjuist. probeer opnieuw.")
+	}
+
+}
+
+
